892b1bec9f93e2bf2f9ff4535f1b07ff1a4db7e9
einarlogi <einarlogi14@gmail.com>
2015-03-13 17:31:08 +0000

    ves

--- EvalAPINode/config/config.js
+++
@@ -1,8 +0,0 @@
-var nconf = require('nconf');
-
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-nconf.argv().env();
-
-nconf.file(__dirname + '/env/' + process.env.NODE_ENV + '.json');
-module.exports = nconf.stores.file.store;
