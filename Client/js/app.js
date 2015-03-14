ab263f17cdbab68deb3609e69a195e44dc3ae75e
Vignir Karl Gylfason <vignirk@gmail.com>
2015-03-14 21:31:49 +0000

    user can now log in

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,13 +1,13 @@
 var EvalClient = angular.module('EvalClient',['ngRoute']);
-angular.module('EvalApp').constant('SERVER_URL', 'http://dispatch.ru.is/h33/api/v1/');
 
-angular.module('EvalClient').factory('LoginResource',['$http', 'SERVER_URL', function($http, SERVER_URL){
+//angular.module('EvalClient').constant('SERVER_URL', 'http://dispatch.ru.is/h33/api/v1/');
+/*angular.module('EvalClient').factory('LoginResource',['$http', 'SERVER_URL', function($http, SERVER_URL){
 	return{
 		postLogin: function (template){
-			$http.post(SERVER_URL + 'login', template);
+			$http.post(SERVER_URL + 'login/', template);
 		}
 	}
-}]);
+}]);*/
 
 
 angular.module('EvalClient').config(
@@ -21,8 +21,8 @@ angular.module('EvalClient').config(
 	}
 ]);
 
-angular.module('EvalClient').controller('LoginController',['$scope', '$location', '$rootScope', '$routeParams', 'LoginResource', 
-	function ($scope, $location, $rootScope, $routeParams, LoginResource){
+angular.module('EvalClient').controller('LoginController',['$scope', '$location', '$rootScope', '$routeParams', '$http', 
+	function ($scope, $location, $rootScope, $routeParams, $http){
 		console.log("login");
 		$scope.user = '';
 		$scope.password = '';
@@ -38,10 +38,9 @@ angular.module('EvalClient').controller('LoginController',['$scope', '$location'
             		user: $scope.user,
             		pass: $scope.password
             	};
-
-            	LoginResource.postLogin(obj).success(function(data){
-					console.log('vei');
-				});
+            	$http.post("http://dispatch.ru.is/h33/api/v1/login", obj).success(function(data){
+            		console.log("dosi hoe");
+            	});
             }
 		};
 }]);
