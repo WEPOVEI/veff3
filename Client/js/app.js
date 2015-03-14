6d0b5d0835577ccf53110da49db474e86aa3823a
einarlogi <einarlogi14@gmail.com>
2015-03-14 23:02:11 +0000

    views

--- Client/js/app.js
+++ Client/js/app.js
@@ -16,6 +16,7 @@ angular.module('EvalClient').config(
 		$routeProvider
 			.when('/login', { templateUrl: 'views/login.html', controller: 'LoginController' })
 			.when('/student/:user/', { templateUrl: 'views/student.html', controller: 'StudentController'})
+			.when('/admin/:admin/', { templateUrl: 'views/admin.html', controller: 'AdminController'})
 			.otherwise({
 	  			redirectTo: '/login'
 			});
@@ -27,6 +28,7 @@ angular.module('EvalClient').controller('LoginController',['$scope', '$location'
 		console.log("login");
 		$scope.user = '';
 		$scope.password = '';
+		$scope.admin = '';
 		$scope.login = function() {
             if($scope.user === ''|| $scope.password === ''){
             	console.log("error msg");
@@ -39,18 +41,28 @@ angular.module('EvalClient').controller('LoginController',['$scope', '$location'
             		user: $scope.user,
             		pass: $scope.password
             	};
-            	
-            	$http.post("http://dispatch.ru.is/h33/api/v1/login", obj).success(function(data){
-            		console.log("dosi hoe");
-            		$location.path(/student/ + $scope.user);
 
-            		console.log(data.User.Role);
+            	$http.post("http://dispatch.ru.is/h33/api/v1/login", obj).success(function(data){
+
+            		console.log("dosi hoe");
+            		if(data.User.Role === 'student'){
+            			$location.path(/student/ + $scope.user);
+            		}
+            		else if(data.User.Role === 'admin'){
+            			$location.path(/admin/ + $scope.admin);
+            		}
+            		//console.log(data.User.Role);
             	});
             }
 		};
 }]);
 
 angular.module('EvalClient').controller('StudentController', ['$scope', '$location', '$rootScope', '$routeParams', '$http', 
+	function ($scope, $location, $rootScope, $routeParams, $http){
+		
+	}]);
+
+angular.module('EvalClient').controller('AdminController', ['$scope', '$location', '$rootScope', '$routeParams', '$http', 
 	function ($scope, $location, $rootScope, $routeParams, $http){
 		
 	}]);
