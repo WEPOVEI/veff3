df0724d7095eb693f7eecae668f7c9e990d3f11a
einarlogi <einarlogi14@gmail.com>
2015-03-14 20:40:52 +0000

    commit for viggi

--- Client/js/app.js
+++ Client/js/app.js
@@ -1,4 +1,13 @@
 var EvalClient = angular.module('EvalClient',['ngRoute']);
+angular.module('EvalApp').constant('SERVER_URL', 'http://dispatch.ru.is/h33/api/v1/');
+
+angular.module('EvalClient').factory('LoginResource',['$http', 'SERVER_URL', function($http, SERVER_URL){
+	return{
+		postLogin: function (template){
+			$http.post(SERVER_URL + 'login', template);
+		}
+	}
+}]);
 
 
 angular.module('EvalClient').config(
@@ -12,7 +21,8 @@ angular.module('EvalClient').config(
 	}
 ]);
 
-angular.module('EvalClient').controller('LoginController',['$scope', '$location', '$rootScope', '$routeParams', function ($scope, $location, $rootScope, $routeParams){
+angular.module('EvalClient').controller('LoginController',['$scope', '$location', '$rootScope', '$routeParams', 'LoginResource', 
+	function ($scope, $location, $rootScope, $routeParams, LoginResource){
 		console.log("login");
 		$scope.user = '';
 		$scope.password = '';
@@ -23,6 +33,41 @@ angular.module('EvalClient').controller('LoginController',['$scope', '$location'
             else{
             	console.log("User: " + $scope.user);
             	console.log("Pass: " + $scope.password);
+
+            	var obj = {
+            		user: $scope.user,
+            		pass: $scope.password
+            	};
+
+            	LoginResource.postLogin(obj).success(function(data){
+					console.log('vei');
+				});
             }
 		};
 }]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
