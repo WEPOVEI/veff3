751ae5045989e67ca3737b8266e2232f1380e053
Vignir Karl Gylfason <vignirk@gmail.com>
2015-03-14 13:29:28 +0000

    Getting ready to login

--- Client/js/app.js
+++ Client/js/app.js
@@ -14,4 +14,15 @@ angular.module('EvalClient').config(
 angular.module('EvalClient').controller('LoginController', ['$scope', '$location', '$rootScope', '$routeParams', 
 	function ($scope, $location, $rootScope, $routeParams){
 		console.log("login");
+		$scope.user = '';
+		$scope.password = '';
+		$scope.login = function() {
+            if($scope.user === ''|| $scope.password === ''){
+            	console.log("error msg");
+            }
+            else{
+            	console.log("User: " + $scope.user);
+            	console.log("Pass: " + $scope.password);
+            }
+		};
 }]);
